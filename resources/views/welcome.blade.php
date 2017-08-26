031fd5e7c66d55b63f0e7223929f244f8c044637
Brad Goldsmith <bradgoldsmith@Brads-iMac.fios-router.home>
2017-08-26 18:06:44 -0400

    maybe maybe maybe

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,6 +15,11 @@
         <link href="{{ asset('css/fullcalendar.min.css') }}" rel="stylesheet">
         <link href="{{ asset('css/fullcalendar.print.min.css') }}" rel="stylesheet">
         <link href="{{ asset('css/visualize.css') }}" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" 
+          crossorigin="anonymous">
+
+          
 
     </head>
     <body>
@@ -587,7 +592,10 @@
 
 
 
-
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" 
+        crossorigin="anonymous">
+      </script>
       <script src="{{ asset('js/jquery.min.js') }}"></script>
       <script src="{{ asset('js/dat.gui.min.js') }}"></script>
       <script src="{{ asset('js/app.js') }}"></script>
