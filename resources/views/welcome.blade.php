e945b16b562b0a07642738b18672af61659a836c
Brad Goldsmith <bradgoldsmith@Brads-iMac.fios-router.home>
2017-08-26 18:12:22 -0400

    not sure

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,7 +18,22 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" 
           crossorigin="anonymous">
-
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" 
+        crossorigin="anonymous">
+      </script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+      <script src="{{ asset('js/jquery.min.js') }}"></script>
+      <script src="{{ asset('js/dat.gui.min.js') }}"></script>
+      <script src="{{ asset('js/app.js') }}"></script>
+      <script src="{{ asset('js/visualize.js') }}"></script>
+      <script src="{{ asset('js/ondemand.js') }}"></script>
+      <script src="{{ asset('js/player.js') }}"></script>
+      <script src="{{ asset('js/request.js') }}"></script>
+      <script src="{{ asset('js/streaminfo.js') }}"></script>
+      <script src="{{ asset('js/moment.min.js') }}"></script>
+      <script src="{{ asset('js/fullcalendar.min.js') }}"></script>
+      <script src="{{ asset('js/main.js') }}"></script>
           
 
     </head>
@@ -587,26 +602,5 @@
           </div>
         </div>
       </div>
-
-
-
-
-
-      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
-        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" 
-        crossorigin="anonymous">
-      </script>
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-      <script src="{{ asset('js/jquery.min.js') }}"></script>
-      <script src="{{ asset('js/dat.gui.min.js') }}"></script>
-      <script src="{{ asset('js/app.js') }}"></script>
-      <script src="{{ asset('js/visualize.js') }}"></script>
-      <script src="{{ asset('js/ondemand.js') }}"></script>
-      <script src="{{ asset('js/player.js') }}"></script>
-      <script src="{{ asset('js/request.js') }}"></script>
-      <script src="{{ asset('js/streaminfo.js') }}"></script>
-      <script src="{{ asset('js/moment.min.js') }}"></script>
-      <script src="{{ asset('js/fullcalendar.min.js') }}"></script>
-      <script src="{{ asset('js/main.js') }}"></script>
     </body>
 </html>
