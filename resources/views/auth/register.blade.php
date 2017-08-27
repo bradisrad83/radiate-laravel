563073298f782aab900efd6e1cb237e22ccdb8d8
Brad Goldsmith <bradgoldsmith@Brads-iMac.fios-router.home>
2017-08-27 10:54:31 -0400

    had to add a stop to the register blade

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>You do not have permission to view this page</h1>
+<div class="container">
+    <h1>You do not have permission to view this page</h1> 
+</div>
 @endsection
+@stop
