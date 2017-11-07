74f0ea8848abe2564ddd764830ad2ee9ee6a12ec
Brad Goldsmith <bradgoldsmith@Brads-iMac.fios-router.home>
2017-11-07 11:58:10 -0500

    not sure whats happending with importing css but gonna do some debugging

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
         <link href="{{ asset('css/visualize.css') }}" rel="stylesheet">
 
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+        @yield('css')
 
     <style>
         html, body {
